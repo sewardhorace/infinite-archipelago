55896d3388d6e6b1bf3cb6bc9d9fceff5655f2d7
Max White <maxjwhite22@gmail.com>
2017-06-27 21:17:23 -0400

    ajax on homepage generators

--- generator/urls.py
+++ generator/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'generator'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^ajax/generate/$', views.generate, name='generate'),
 ]
--- generator/views.py
+++ generator/views.py
@@ -1,6 +1,9 @@
 import os, json, random
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, JsonResponse
+from django.urls import reverse
+from django.core import serializers
 
 from infinitelabyrinth.settings import BASE_DIR
 from core.namer import CharacterNamer
@@ -22,14 +25,69 @@ namer = CharacterNamer(names_corpus)
 
 
 def index(request):
-    format_string = ('{Subject} is a {race} {char_detail_physical}.\n'
-        '{Subject} {char_trait_attitude} and {char_trait_manner}.\n'
-        '{Subject} {char_ability_competency} and also {char_ability_move}.\n'
-        '{Subject} is {char_inventory}.')
+    char_format_string = ('- {race} {char_detail_physical}.\n'
+        '- {subject} {char_trait_attitude}.\n'
+        '- {subject} {char_trait_manner}.\n'
+        '- {subject} {char_ability_competency}.\n'
+        '- {subject} {char_ability_move}.\n'
+        '- {subject} is {char_inventory}.')
 
-    name = scrambler.scramble(namer.full_name_string())
-    description = scrambler.scramble(format_string, random.choice(['m', 'f']))
+    char_name = scrambler.scramble(namer.full_name_string())
+    char_description = scrambler.scramble(char_format_string, random.choice(['m', 'f']))
+    character = Character(char_name=char_name, char_description=char_description)
 
-    character = Character(char_name=name, char_description=description)
-    context = {'character': character}
+    room_format_string = ('- the room is 25ft x 25ft.\n'
+        '- {construction}.\n'
+        '- {atmosphere}.\n'
+        '- {feature}.')
+    room_description = scrambler.scramble(room_format_string)
+    room = room_description
+
+    loot_format_string = '{item_misc}'
+    loot = scrambler.scramble(loot_format_string)
+
+    context = {
+        'character': character,
+        'room': room,
+        'loot': loot,
+    }
     return render(request, 'generator/index.html', context)
+
+def generate(request):
+    data = {}
+    if request.GET.get('type') == 'character':
+        char_format_string = ('- {race} {char_detail_physical}.\n'
+            '- {subject} {char_trait_attitude}.\n'
+            '- {subject} {char_trait_manner}.\n'
+            '- {subject} {char_ability_competency}.\n'
+            '- {subject} {char_ability_move}.\n'
+            '- {subject} is {char_inventory}.')
+
+        char_name = scrambler.scramble(namer.full_name_string())
+        char_description = scrambler.scramble(char_format_string, random.choice(['m', 'f']))
+        data = {
+            'type': 'character',
+            'character': {
+                'char_name':char_name,
+                'char_description': char_description
+            }
+        }
+    elif request.GET.get('type') == 'room':
+        room_format_string = ('- the room is 25ft x 25ft.\n'
+        '- {construction}.\n'
+        '- {atmosphere}.\n'
+        '- {feature}.')
+        room_description = scrambler.scramble(room_format_string)
+        data = {
+            'type': 'room',
+            'room': room_description
+        }
+    elif request.GET.get('type') == 'loot':
+        loot_format_string = '{item_misc}'
+        loot_description = scrambler.scramble(loot_format_string)
+        data = {
+            'type': 'loot',
+            'loot': loot_description
+        }
+    # data = serializers.serialize("json", data)
+    return JsonResponse(data)
